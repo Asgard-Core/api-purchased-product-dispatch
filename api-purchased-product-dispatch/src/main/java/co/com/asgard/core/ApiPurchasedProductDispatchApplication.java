be21a1a0ee9998763e8e5b7daf552a9a8152504e
Jhon Riaño <44453281+Jrianio@users.noreply.github.com>
2025-03-03 21:36:44 -0500

    Add files via upload

---
+++ api-purchased-product-dispatch/src/main/java/co/com/asgard/core/ApiPurchasedProductDispatchApplication.java
@@ -0,0 +1,13 @@
+package co.com.asgard.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPurchasedProductDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPurchasedProductDispatchApplication.class, args);
+	}
+
+}
---
+++ api-purchased-product-dispatch/src/test/java/co/com/asgard/core/ApiPurchasedProductDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.asgard.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPurchasedProductDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
