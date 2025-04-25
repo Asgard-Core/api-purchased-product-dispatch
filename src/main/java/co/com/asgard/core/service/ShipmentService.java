9feb4e16fcf1d869ac2b0de155ff60190db07d32
JhonnyDiaz4753 <jhonnydiaz@estudiante.uniajc.edu.co>
2025-04-24 20:07:28 -0500

    commit Jhonny: HU 4 completa

--- src/main/java/co/com/asgard/core/service/ShipmentService.java
+++ src/main/java/co/com/asgard/core/service/ShipmentService.java
@@ -1,5 +1,7 @@
 package co.com.asgard.core.service;
 import co.com.asgard.core.dto.AssignedOrderDTO;
+import co.com.asgard.core.repository.CustomerOrderRepository;
+import co.com.asgard.core.repository.OrderStatusRepository;
 import co.com.asgard.core.repository.ShipmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,4 +17,25 @@ public class ShipmentService {
     public List<AssignedOrderDTO> getAssignedOrders(Long carrierId) {
         return shipmentRepository.findAssignedOrdersByCarrier(carrierId);
     }
+
+    @Autowired
+private CustomerOrderRepository customerOrderRepository;
+
+@Autowired
+private OrderStatusRepository orderStatusRepository;
+
+public boolean updateOrderStatus(Long orderId, Long statusId) {
+    var optionalOrder = customerOrderRepository.findById(orderId);
+    var optionalStatus = orderStatusRepository.findById(statusId);
+
+    if (optionalOrder.isEmpty() || optionalStatus.isEmpty()) {
+        return false;
+    }
+
+    var order = optionalOrder.get();
+    order.setStatus(optionalStatus.get());
+    customerOrderRepository.save(order);
+    return true;
+}
+
 }
